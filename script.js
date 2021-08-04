e6552150efbc45499a0a41b797f9b519631cc3c5
Jose Lopes <946393361bill@gmail.com>
2021-08-04 20:13:30 +0100

    criando o método prevent default

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.busca').addEventListener('submit', (event) => {
+    event.preventDefault(); // previnir evento padrão (mantem as informações no formulário ao submeter)
+
+});
+
