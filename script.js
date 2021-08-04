0d855536b6aa7fa505196986792c0b1e9258ed01
Jose Lopes <946393361bill@gmail.com>
2021-08-04 20:28:55 +0100

    input, showWarning e url da API

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 document.querySelector('.busca').addEventListener('submit', (event) => {
     event.preventDefault(); // previnir evento padrão (mantem as informações no formulário ao submeter)
 
+    const input = document.querySelector('#searchInput').value;
+    
+    if(input !== '') {
+        showWarning('Carregando...')
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
+
 });
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg;
+}
 
