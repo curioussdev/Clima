a70a7e2708977c39963bbcafc696fb3daad7de87
Jose Lopes <946393361bill@gmail.com>
2021-08-05 15:42:33 +0100

    criando a função results

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
 
         const url = `api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=${process.env.APP_WEATHER_API_KEY}&units=metric&lang=pt_br`;
 
-    }
+        const results = await fetch(url);
+    };
 
     
 });
